234468dcbbead2061e5284d1a29f2cba39b45403
Juha Penttilä <juha.ea.penttila@gmail.com>
2020-03-05 12:08:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@ require 'yaml'
 def load_library(file_path)
   library = YAML.load_file(file_path)
   result = {"get_meaning" => {}, "get_emoticon" => {}} 
-  library ea
+  library.each do 
   
   
 end
