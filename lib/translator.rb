c5d93eef14c8c2d5d63603e31031b28626ace605
Juha Penttilä <juha.ea.penttila@gmail.com>
2020-03-05 12:19:58 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,7 +13,10 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   library = load_library(file_path)
-  if library["get_emoticon"].include?  
+  if library["get_emoticon"].include? 
+    library["get emoticon"]
+  else 
+    
   
 end
 
