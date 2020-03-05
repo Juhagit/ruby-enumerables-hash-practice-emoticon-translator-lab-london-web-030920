351347f199ae4dbb2cf8ae28cf29e698f5f6a7d9
Juha Penttilä <juha.ea.penttila@gmail.com>
2020-03-05 12:31:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,12 +13,12 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   library = load_library(file_path)
- library["get_emoticon"].each do |english, japanese|
-    if emoticon == english
-      response = japanese
-    else 
+
+  if library["get_emoticon"].include?(emoticon) 
+    library["get_emoticon"][emoticon]
+  else
     "Sorry, that emoticon was not found"
-  end 
+  end
 end
 
 def get_english_meaning
